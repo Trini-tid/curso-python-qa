80d8f43523aefb2ad9c5a90d97d28992ad3ffa20
Trinidad Diez Rojo <trinidad.diezrojo@capgemini.com>
2025-12-17 02:13:36 +0100

    Add files via upload

---
+++ unittest_exercices/exer-1.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TextExample(unittest.TestCase):
+    
+    def test_assertion(self):
+        self.assertEqual("some string", "some other")
+        self.a
+        
+unittest.main(argv=[''], verbosity=2, exit=False)
